b8402107bbb2e79c527d2a6d1d01b5145ce8f24a
Sahar <bellem75@yahoo.com>
2024-02-22 13:52:12 -0800

    finish the header section and make it responsive in all devices

---
+++ sahar-mahdi-brainflix/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+// Import the Header section from Header component
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ sahar-mahdi-brainflix/src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import '../Header/Header.scss';
+import image from '../../assets/Logo/BrainFlix-logo.svg';
+import searchIcon from '../../assets/Icons/search.svg';
+import avatar from '../../assets/Images/Mohan-muruge.jpg';
+import uploadIcon from '../../assets/Icons/upload.svg';
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='header__container'>
+        <div className='header__logo'>
+          <img className='header__logo--brainflix' src={image} alt='logo' />
+        </div>
+
+        <div className='header__subcontainer'>
+          <div className='header__search'>
+            <img className='header__icon' src={searchIcon} alt='search icon' />
+            <input className='header__input' type='text' placeholder='Search' />
+          </div>
+          <div className='header__upload'>
+            <img
+              className='header__uploadicon'
+              src={uploadIcon}
+              alt='upload img'
+            />
+            <button className='header__btn'>UPLOAD </button>
+          </div>
+          <img className='header__avatar' src={avatar} alt='avatar img' />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
