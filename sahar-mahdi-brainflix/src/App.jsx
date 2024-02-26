750596e9368dc4234f4604f272555012461d8143
Sahar <bellem75@yahoo.com>
2024-02-25 17:43:02 -0800

    finish the last section

--- sahar-mahdi-brainflix/src/App.jsx
+++ sahar-mahdi-brainflix/src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header/Header';
 import Video from './components/Video/Video';
 import videoData from '../src/data/video-details.json';
 import Comments from '../src/components/Comments/Comments';
+import Youtube from '../src/components/Youtube/Youtube';
+// import youtubeData from '../src/data/videos.json';
 
 import { useState } from 'react';
 
@@ -15,13 +17,17 @@ console.log(setSelectedVideo);
 
 const [selectComments, setSelectComment] = useState(videoData[0].comments.slice(0,3));
 console.log(setSelectComment);
-console.log('selectComments', selectComments);
+
+const [selectYoutubeVideo, setSelectYoutubeVideo] = useState(videoData.slice(1));
+console.log(setSelectYoutubeVideo);
+console.log('youtube', selectYoutubeVideo);
 
   return (
     <div className="App">
     <Header />
 	<Video selectVideo={selectedVideo} />
     <Comments selectComment={selectComments} />
+	<Youtube selectYoutube={selectYoutubeVideo} />
 	</div>
   );
 }
---
+++ sahar-mahdi-brainflix/src/components/Youtube/Youtube.jsx
@@ -0,0 +1,21 @@
+import './Youtube.scss';
+
+function Youtube(props) {
+  return (
+    <div className='youtube'>
+      <h4 className='youtube__title'>NEXT VIDEOS</h4>
+
+      {props.selectYoutube.map((youtubeWatch) => (
+        <div className='youtube__container' key={youtubeWatch.id}>
+          <img className='youtube__image' src={youtubeWatch.image} alt='imge' />
+
+          <div className='youtube__subcontainer'>
+            <h4 className='youtube__subtitle'>{youtubeWatch.title}</h4>
+            <p className='youtube__para'>{youtubeWatch.channel}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Youtube;
