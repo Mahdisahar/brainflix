2b7dd7580bfb10128de3b2ae1dd9411922b6cc48
Sahar <bellem75@yahoo.com>
2024-02-27 16:44:27 -0800

    finish the upload page in mobile device

--- sahar-mahdi-brainflix/src/pages/Videopage/Videopage.jsx
+++ sahar-mahdi-brainflix/src/pages/Videopage/Videopage.jsx
@@ -1,8 +1,37 @@
 import './Videopage.scss';
+import uploadVideoPreview from '../../assets/Images/Upload-video-preview.jpg';
+import publishIcon from '../../assets/Icons/publish.svg';
 
 function Videopage () {
 	return (
-		<div>Hi</div>
+		<section className="upload">
+			<h2 className='upload__title'>Upload Video</h2>	
+		<p className='upload__para'>VIDEO THUMBNAIL</p>
+		<img className='upload__image' src={uploadVideoPreview} alt="thumbnail" />
+		<form className="upload__detail">
+			<div className='upload__info'>
+			<label className='upload__label' htmlFor="text">TITLE YOUR VIDEO</label>
+			<input className='upload__input' type="text" placeholder='Add a title to your video' />
+
+			</div>
+			<div className='upload__info'>
+			<label className='upload__label' htmlFor="text">ADD A VIDEO DESCRIPTION</label>
+			<textarea className='upload__textarea' type="text" placeholder='Add a description to your video' />
+			</div>
+
+			<div className='upload__button'>
+			<div className='upload__published'>
+			<img className='upload__publishedicon' src={publishIcon} alt="publish icon" />
+			<button className='upload__publish'>PUBLISH</button>
+			</div>
+				
+			<button className='upload__cancel'>CANCEL</button>
+			</div>
+		
+			
+		</form>
+		</section>
+	
 
 	);
 }
